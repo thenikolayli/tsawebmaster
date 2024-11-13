5e95df05b9e6e471c117512d1f08644b356fd96e
Saurish <saurish.sriv@gmail.com>
2024-11-12 23:44:11 -0800

    Made it so that the topbar will now change color when passing a specific part of the page-have some issues will discuss soon.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,51 +1,80 @@
-import { useEffect } from "react";
-import Topbar from "../components/Topbar.tsx";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import Footer from "./footer.tsx";
+import { createPortal } from "react-dom";
+import { useState, useEffect } from "react";
 
-const Home = () => {
+const Topbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+    const [textColor, setTextColor] = useState('text-sbeige');  // Default color
+
     useEffect(() => {
-        document.title = "Home";
+        const handleScroll = () => {
+            if (window.scrollY > 830) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
 
-    const logo = "./src/assets/olivedelights-logo.png";
-    const imageSource =
-        "https://d303frzni7t4jb.cloudfront.net/2024_11_11_ea507918-4eb8-4a23-8014-8a7b9506daf4?Expires=1731652698&Signature=CRC~hPJf-lrSfjVk7ZwDUFbWHgHSGQkRiQ6LQaeRSmFUckbiJbVa8TpNyHrX2bWenx67TPQ~5Ei6p4hwMFlq~4ECwpXE5nWJzuBy-L2reUdJH9M10DocW9gvCrKREIx7ADXOagaw4-I8C~a1XgUEtCSC86Oc3j-o-8zAI822l39-q70F~f1dRxRyiZiCQZbawjXvXqwad6KYu~B0fMHt1zXZo7EUF3NnCM1ALgrKXAjNIW5uKv1O6-o~JgPV7rvnRRSaT--ZRa~sfMcm1pv-hn76pcl6tauk2npJg0Th5AzAF9MG~kfovKXJ9kotDLrvKwlWhHxwyLSTd5IAdQesUA__&Key-Pair-Id=KTUQ5O0UDZ92B";
+    useEffect(() => {
+        // Detect the color change based on scroll or some other condition
+        if (scrolled) {
+            // If scrolled and the color is not black, change it to white
+            setTextColor('text-white');
+        } else {
+            // Otherwise, revert to default
+            setTextColor('text-sbeige');
+        }
+    }, [scrolled]);
 
-    return (
-        <>
-            <Topbar />
-            <div className="bg-sbeige min-h-screen">
-                <div className="absolute w-full max-h-[96%] overflow-hidden aspect-video">
-                    <img
-                        className="z-0 absolute w-full object-cover"
-                        src={imageSource}
-                        alt="Yummy food"
-                    />
-                    <img
-                        className="z-10 absolute w-3/5 inset-0 mx-auto top-[25%]"
-                        src={logo}
-                        alt="Logo"
-                    />
-                    <div className="z-20 absolute w-fit h-fit inset-0 mx-auto top-[80%] flex space-x-4">
-                        <button className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
-                            <FontAwesomeIcon icon={faClock} className="text-olivegreen fill-current" />
-                            <span className="text-olivegreen">Reserve</span>
-                        </button>
-                        <button className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
-                            <FontAwesomeIcon icon={faArrowRight} className="text-olivegreen fill-current" />
-                            <span className="text-olivegreen">Order</span>
-                        </button>
-                    </div>
-                </div>
+    return createPortal(
+        <div className="relative">
+            {/* Invisible rectangle below the topbar */}
+            <div className="absolute top-[64px] left-0 w-full h-16 bg-transparent pointer-events-none z-20"></div>
+
+            {/* Topbar content */}
+            <div
+                className={`fixed top-0 left-0 w-full z-30 grid-cols-3 space-x-20 rounded-full p-4 transition-all duration-150 ${textColor}`}
+            >
+                <a
+                    href="/"
+                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
+                >
+                    Home
+                </a>
+                <a
+                    href="/loco"
+                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
+                >
+                    Locations
+                </a>
+                <a
+                    href="/menu"
+                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
+                >
+                    Menu
+                </a>
+                <a
+                    href="/aboutus"
+                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
+                >
+                    About Us
+                </a>
+                <a
+                    href="/careers"
+                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
+                >
+                    Careers
+                </a>
             </div>
-
-            <div className="bg-sbeige h-[3000px]"></div>
-
-            <Footer />
-        </>
+        </div>,
+        document.getElementById("sidebar")!
     );
 };
 
-export default Home;
+export default Topbar;
