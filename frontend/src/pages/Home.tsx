19da1b488725d64f18b4f370b07ac888d4253173
thenikolayli <nikolay.li2008@gmail.com>
2024-11-13 19:01:55 -0800

    updated about me, added gifs

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,75 +1,50 @@
-import { createPortal } from "react-dom";
-import { useState, useEffect } from "react";
+import Topbar from "../components/Topbar.tsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {useEffect} from "react";
+import Footer from "./Footer.tsx";
 
-const Topbar = () => {
-    const [scrolled, setScrolled] = useState(false);
-    const [textColor, setTextColor] = useState('text-sbeige');  // Default color
-
+const Home = () => {
     useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 830) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
+        document.title = "Home";
     }, []);
 
-    useEffect(() => {
-        if (scrolled) {
-            setTextColor('text-white');
-        } else {
-            setTextColor('text-sbeige');
-        }
-    }, [scrolled]);
+    const logo = "./src/assets/images/olivedelights-logo.png";
+    const imageSource = "./src/assets/gifs/grilling-frying.gif";
 
-    return createPortal(
-        <div className="relative">
-            <div className="absolute top-[64px] left-0 w-full h-16 bg-transparent pointer-events-none z-20"></div>
-
-            <div
-                className={`fixed top-0 left-0 w-full z-30 grid-cols-3 space-x-20 rounded-full p-4 transition-all duration-150 ${textColor}`}
-            >
-                <a
-                    href="/"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Home
-                </a>
-                <a
-                    href="/loco"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Locations
-                </a>
-                <a
-                    href="/menu"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Menu
-                </a>
-                <a
-                    href="/aboutus"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    About Us
-                </a>
-                <a
-                    href="/careers"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Careers
-                </a>
+    return (
+        <>
+            <Topbar />
+            <div className="bg-sbeige min-h-screen">
+                <div className="absolute w-full max-h-[96%] overflow-hidden aspect-video">
+                    <img
+                        className="z-0 absolute w-full object-cover"
+                        src={imageSource}
+                        alt="Yummy food"
+                    />
+                    <img
+                        className="z-10 absolute w-3/5 inset-0 mx-auto top-[25%]"
+                        src={logo}
+                        alt="Logo"
+                    />
+                    <div className="z-20 absolute w-fit h-fit inset-0 mx-auto top-[80%] flex space-x-4">
+                        <button className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
+                            <FontAwesomeIcon icon={faClock} className="text-olivegreen fill-current" />
+                            <span className="text-olivegreen">Reserve</span>
+                        </button>
+                        <button className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
+                            <FontAwesomeIcon icon={faArrowRight} className="text-olivegreen fill-current" />
+                            <span className="text-olivegreen">Order</span>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>,
-        document.getElementById("sidebar")!
+
+            <div className="bg-sbeige h-[3000px]"></div>
+
+            <Footer />
+        </>
     );
 };
 
-export default Topbar;
+export default Home;
--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -3,7 +3,7 @@ import Topbar from "../components/Topbar.tsx";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 // import {faArrowRight, faClock} from "@fortawesome/free-solid-svg-icons";
-import Footer from "./footer.tsx"
+import Footer from "./Footer.tsx"
 
 const Menu = () => {
     useEffect(() => {
