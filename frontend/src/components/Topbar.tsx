3b11629e8482ec0b471378912849c9abfa51c881
Saurish <saurish.sriv@gmail.com>
2024-11-16 17:10:50 -0800

    make the topbar responsive at footer at the bottom

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -6,11 +6,7 @@ const Topbar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 830) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
+            setScrolled(window.scrollY > 830);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -21,45 +17,122 @@ const Topbar = () => {
     }, []);
 
     return createPortal(
-        <div className="relative z-30">
+        <div className="relative">
             <div className="absolute top-full left-0 w-full h-16 bg-transparent pointer-events-none"></div>
 
             <div
-                className={`grid-cols-3 space-x-20 rounded-full p-4 transition-all duration-150 ${scrolled ? 'text-black' : 'text-sbeige'}`}
+                className={`hidden lg:flex lg:justify-center lg:space-x-20 lg:rounded-full lg:p-4 lg:transition-all lg:duration-150 ${
+                    scrolled ? "lg:text-black" : "lg:text-sbeige"
+                } w-full max-w-screen-xl mx-auto`}
             >
-                <a
-                    href="/"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
+                <ul className="flex lg:space-x-[100px]">
+                    <a
+                        href="/"
+                        className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white "
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="/loco"
+                        className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white "
+                    >
+                        Locations
+                    </a>
+                    <a
+                        href="/menu"
+                        className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white "
+                    >
+                        Menu
+                    </a>
+                    <a
+                        href="/aboutus"
+                        className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white "
+                    >
+                        About Us
+                    </a>
+                    <a
+                        href="/careers"
+                        className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white "
+                    >
+                        Careers
+                    </a>
+                </ul>
+            </div>
+
+            <div className={`lg:hidden fixed top-0 left-0 z-[1000]`}>
+                <input type="checkbox" id="sidebar-active" className="hidden peer" />
+
+                {/* Hamburger Menu */}
+                <label
+                    htmlFor="sidebar-active"
+                    className="fixed z-[1001] p-4 backdrop: text-white cursor-pointer peer-checked:hidden"
                 >
-                    Home
-                </a>
-                <a
-                    href="/loco"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Locations
-                </a>
-                <a
-                    href="/menu"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Menu
-                </a>
-                <a
-                    href="/aboutus"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    About Us
-                </a>
-                <a
-                    href="/careers"
-                    className="text-xl hover:text-sbeige/50 transition-all duration-150 font-lato text-inherit hover:font-bold hover:text-white"
-                >
-                    Careers
-                </a>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="45px"
+                        viewBox="0 -960 960 960"
+                        width="45px"
+                        fill="#FFFFFF"
+                        className="transition-all duration-300 ease-in-out"
+                    >
+                        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                    </svg>
+                </label>
+
+                <div className="fixed inset-y-0 left-0 w-[95%] bg-blueGray-900 text-white transform -translate-x-full transition-transform duration-300 peer-checked:translate-x-0 z-[1000]">
+                    <div className="p-6">
+                        <label
+                            htmlFor="sidebar-active"
+                            className="cursor-pointer text-gray-400 hover:text-white"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="45px"
+                                viewBox="0 -960 960 960"
+                                width="45px"
+                                fill="#FFFFFF"
+                                className="transition-all duration-300 ease-in-out"
+                            >
+                                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                            </svg>
+                        </label>
+                    </div>
+                    <nav className={`flex flex-col p-6 space-y-4`}>
+                        <a
+                            href="/"
+                            className="text-lg font-medium hover:text-gray-300"
+                        >
+                            Home
+                        </a>
+                        <a
+                            href="/loco"
+                            className="text-lg font-medium hover:text-gray-300"
+                        >
+                            Locations
+                        </a>
+                        <a
+                            href="/menu"
+                            className="text-lg font-medium hover:text-gray-300"
+                        >
+                            Menu
+                        </a>
+                        <a
+                            href="/aboutus"
+                            className="text-lg font-medium hover:text-gray-300"
+                        >
+                            About Us
+                        </a>
+                        <a
+                            href="/careers"
+                            className="text-lg font-medium hover:text-gray-300"
+                        >
+                            Careers
+                        </a>
+                    </nav>
+                </div>
             </div>
         </div>,
-        document.getElementById("sidebar")!
+        document.getElementById("top bar")!
     );
 };
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import {useEffect} from "react";
 import Footer from "./Footer.tsx";
+import {createPortal} from "react-dom";
 
 const Home = () => {
     useEffect(() => {
@@ -10,10 +11,10 @@ const Home = () => {
     }, []);
 
     const logo = "./src/assets/images/olivedelights-logo.png";
-    const imageSource = "./src/assets/gifs/grilling-frying.gif";
+    const imageSource = "./src/assets/gifs/homescreen.gif";
 
-    return (
-        <>
+    return createPortal(
+        <div>
             <Topbar />
             <div className="bg-sbeige min-h-screen">
                 <div className="absolute w-full max-h-[96%] overflow-hidden aspect-video">
@@ -28,12 +29,14 @@ const Home = () => {
                         alt="Logo"
                     />
                     <div className="z-20 absolute w-fit h-fit inset-0 mx-auto top-[80%] flex space-x-4">
-                        <button className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
-                            <FontAwesomeIcon icon={faClock} className="text-olivegreen fill-current" />
+                        <button
+                            className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
+                            <FontAwesomeIcon icon={faClock} className="text-olivegreen fill-current"/>
                             <span className="text-olivegreen">Reserve</span>
                         </button>
-                        <button className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
-                            <FontAwesomeIcon icon={faArrowRight} className="text-olivegreen fill-current" />
+                        <button
+                            className="bg-sbeige text-olivegreen px-6 py-3 rounded-full hover:bg-sbeige/70 transition duration-300 flex items-center justify-center space-x-2">
+                            <FontAwesomeIcon icon={faArrowRight} className="text-olivegreen fill-current"/>
                             <span className="text-olivegreen">Order</span>
                         </button>
                     </div>
@@ -41,9 +44,9 @@ const Home = () => {
             </div>
 
             <div className="bg-sbeige h-[3000px]"></div>
-
-            <Footer />
-        </>
+            <Footer/>
+        </div>,
+    document.getElementById("root")!
     );
 };
 
