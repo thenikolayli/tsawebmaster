a2cd70d908937bd7aa708f64c984b835f58f6be7
thenikolayli <nikolay.li2008@gmail.com>
2024-12-08 16:18:33 -0800

    updated .gitignore and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/static/",
+  build: {
+    outDir: '../backend/dist/',
+  },
   server: {
     port: 3000,
     proxy: {
